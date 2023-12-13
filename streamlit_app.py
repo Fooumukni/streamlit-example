0652d9ece5f64fc92a70bff52b85fa08330b4fe8
Fooname <71482051+Fooumukni@users.noreply.github.com>
2023-12-13 17:55:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,27 @@
-import  math
-from turtle import *
-def hearta(k) :
-       return 15*math.sin(k)**3
-def heartb(k) :
-       return 12*math.cos(k)-5*\
-                math.cos(2*k)-2*\
-                math.cos(3*k)-\
-                math.cos(4*k)
-speed(1000)
-bgcolor("black")
-for i in range(6000):
-    goto(hearta(i)*20,heartb(i)*20)
-    for j in range(5):
-        color('#f73487')
-    goto(0,0)
-done()
+import streamlit as st
+import plotly.express as px
+import numpy as np
+
+def hearta(k):
+    return 15 * np.sin(k)**3
+
+def heartb(k):
+    return 12 * np.cos(k) - 5 * np.cos(2 * k) - 2 * np.cos(3 * k) - np.cos(4 * k)
+
+# Crear datos para el corazón
+theta = np.linspace(0, 2 * np.pi, 6000)
+x = hearta(theta) * 20
+y = heartb(theta) * 20
+
+# Crear figura de Plotly
+fig = px.line(x=x, y=y, line_shape="linear", labels={"x": "", "y": ""})
+
+# Estilo de la figura
+fig.update_layout(
+    showlegend=False,
+    xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+    yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+)
+
+# Mostrar la figura en Streamlit
+st.plotly_chart(fig)
